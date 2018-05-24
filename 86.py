b5e03ac16f03192af7afb401974a40ff3f401e8c
Valentun <karbon4ic@gmail.com>
2018-05-24 16:23:54 +0700

    Fix bugs on set with two or more similar elements

--- 86.py
+++ 86.py
@@ -1,3 +1,5 @@
+from math import factorial
+
 N = int(input())
 
 odd = [0] * 7
@@ -6,23 +8,27 @@ even = [0] * 7
 res = 0
 
 
+def placements(n, k):
+    return 0 if n < k else factorial(n) / (factorial(k) * factorial(n-k))
+
+
 def update_result(i, j, k):
     global res
 
     if i == j == k:
-        res += odd[i] * (odd[i] - 1) * (odd[i] - 2) \
-               + odd[i] * even[i] * (even[i] - 1)
+        res += placements(odd[i], 3) \
+               + odd[i] * placements(even[i], 2)
     elif i == j:
-        res += odd[i] * (odd[i] - 1) * odd[k] \
-               + even[i] * (even[i] - 1) * odd[k] \
+        res += placements(odd[i], 2) * odd[k] \
+               + placements(even[i], 2) * odd[k] \
                + even[i] * odd[i] * even[k]
     elif j == k:
-        res += odd[j] * (odd[j] - 1) * odd[i] \
-               + even[j] * (even[j] - 1) * odd[i] \
+        res += placements(odd[j], 2) * odd[i] \
+               + placements(even[j], 2) * odd[i] \
                + even[j] * odd[j] * even[i]
     elif i == k:
-        res += odd[i] * (odd[i] - 1) * odd[j] \
-               + even[i] * (even[i] - 1) * odd[j] \
+        res += placements(odd[i], 2) * odd[j] \
+               + placements(even[i], 2) * odd[j] \
                + even[i] * odd[i] * even[j]
     else:
         res += odd[i] * odd[j] * odd[k] \
@@ -44,7 +50,7 @@ for i in range(0, 7):
             if (i + j + k) % 7 == 0:
                 update_result(i, j, k)
 
-print(res)
+print(int(res))
 
 # Test data
 # 7
